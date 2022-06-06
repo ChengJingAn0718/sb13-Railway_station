235158adcfec9317035c201488c2b94e1c9e10b3
BlueDream0718 <104906327+Cheng0718@users.noreply.github.com>
2022-06-06 09:55:35 -0800

    v111

--- src/components/App.js
+++ src/components/App.js
@@ -37,6 +37,7 @@ const App = React.forwardRef(({ geo, _startTransition, baseGeo, bgLoaded }, ref)
       sceneList[1].current.sceneLoad()
       sceneList[0].current.sceneLoad()
 
+
       return () => {
       }
     }, []
